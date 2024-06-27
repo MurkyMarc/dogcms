685e1a83c37047eba10a1af0f265193462f13d0d
marc marti <helloimmarc@gmail.com>
2024-06-26 20:51:29 -0700

    added back button

---
+++ src/components/ui/icons/BackButton.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../button';
+import { ArrowLeftIcon } from '@radix-ui/react-icons';
+
+const BackButton = ({ text }: { text?: string }) => {
+    const navigate = useNavigate();
+
+    return (
+        <Button
+            variant="outline"
+            onClick={() => navigate(-1)}
+            className="items-center gap-2 rounded-md px-3 py-2 text-sm font-medium transition-colors hover:bg-muted"
+        >
+            <ArrowLeftIcon className="h-4 w-4" />
+            {text}
+        </Button>
+    );
+};
+
+export default BackButton;
+
+
--- src/pages/Dashboard/components/EditWalk.tsx
+++ src/pages/Dashboard/components/EditWalk.tsx
@@ -3,13 +3,14 @@ import { EditWalkForm } from "../../../components/forms/EditWalkForm";
 import { Separator } from "../../../components/ui/separator";
 import { useGetWalkById } from "../../../api/hooks/useWalks";
 import { errorToast } from "../../../utils/helpers";
+import BackButton from "../../../components/ui/icons/BackButton";
 
 export default function EditWalk() {
     const { id } = useParams();
     const navigate = useNavigate();
     const { data: walk, isLoading } = useGetWalkById(id || "");
 
-    if ( !id && !isLoading) {
+    if (!id && !isLoading) {
         errorToast(new Error("Walk ID not found"));
         navigate("/dashboard/walks");
     }
@@ -19,11 +20,14 @@ export default function EditWalk() {
     return (
         <div className="p-6 space-y-8">
             <div className="items-center">
-                <div>
-                    <h3 className="text-lg font-medium">Edit Walk</h3>
-                    <p className="text-sm text-muted-foreground">
-                        Update the details of your walk.
-                    </p>
+                <div className="inline-flex items-center gap-2">
+                    <BackButton />
+                    <div className="ml-2">
+                        <h3 className="text-lg font-medium">Edit Walk</h3>
+                        <p className="text-sm text-muted-foreground">
+                            Update the details of your walk.
+                        </p>
+                    </div>
                 </div>
                 <Separator className="my-6" />
                 {walk ? <EditWalkForm walk={walk} /> : null}
