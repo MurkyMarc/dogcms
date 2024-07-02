e9a903853baca3e520569c183d06dcf4e0b5a5ca
marc marti <helloimmarc@gmail.com>
2024-07-01 23:45:21 -0700

    updated message bubble style to put the unrounded corner on the bottom instead of the top

--- src/pages/Dashboard/Messages/components/message.tsx
+++ src/pages/Dashboard/Messages/components/message.tsx
@@ -25,7 +25,7 @@ export function Message({ message, isYourMessage, otherUserName, otherUserProfil
                     <div className={cn("flex", isYourMessage ? "flex-row-reverse" : "")}>
                         <div className={cn(
                             "w-fit justify-center border border-gray-200 bg-accent py-2 px-3 rounded-lg max-w-xs sm:max-w-lg",
-                            isYourMessage ? "rounded-tr-none" : "rounded-tl-none"
+                            isYourMessage ? "rounded-br-none" : "rounded-bl-none"
                         )}>
                             {!isYourMessage && <div className="font-bold text-xs capitalize">{otherUserName}</div>}
                             {message.content}
