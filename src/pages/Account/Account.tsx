3fd745790e572313e93b25d5eb8908575c0fcd9b
marc marti <helloimmarc@gmail.com>
2024-09-07 16:36:56 -0700

    added menu button back to account page

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -3,6 +3,7 @@ import { Separator } from "../../components/ui/separator"
 import { SidebarNav } from './components/SidebarNav'
 import { useEffect } from 'react'
 import { useSession } from '../../api/hooks/useAuth'
+import { MenuButton } from '../Dashboard/components/MenuButton'
 
 export const Account = () => {
     const navigate = useNavigate();
@@ -14,12 +15,16 @@ export const Account = () => {
 
     return (
         <div className="space-y-6 p-10 pb-16 md:block">
-            <div className="space-y-0.5">
-                <h2 className="text-2xl font-bold tracking-tight">My Account</h2>
-                <p className="text-muted-foreground">
-                    Manage your account settings and set preferences.
-                </p>
+            <div className="flex items-center">
+                <MenuButton className="mr-4" />
+                <div className="space-y-0.5">
+                    <h2 className="text-2xl font-bold tracking-tight">My Account</h2>
+                    <p className="text-muted-foreground hidden lg:block">
+                        Manage your account settings and set preferences.
+                    </p>
+                </div>
             </div>
+
             <Separator className="my-6" />
             <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
                 <aside className="lg:w-1/5">
